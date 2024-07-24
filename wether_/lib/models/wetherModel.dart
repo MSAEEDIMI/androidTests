eb215e5ac2d56ba1945d74d8ce694a1c538946b0
mohammad-saeedi <120755947+MSAEEDIMI@users.noreply.github.com>
2024-07-24 14:35:32 +0330

    add wether app

---
+++ wether_/lib/models/wetherModel.dart
@@ -0,0 +1,14 @@
+class wetherData{
+  String? location,description,icon;
+  dynamic temp ,feelslike,humidity,wind;
+
+  wetherData({
+    this.location,
+    this.description,
+    this.icon,
+    this.temp,
+    this.feelslike,
+    this.humidity,
+    this.wind,
+  });
+}
